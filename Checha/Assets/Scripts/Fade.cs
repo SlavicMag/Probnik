287ea00352fda3f96fcb1754e713b62525b6f95f
SlavicMag <govoruxadaniil@gmail.com>
2026-01-20 21:07:51 +0300

    стиль
    вапвыпыв

--- Checha/Assets/Scripts/Fade.cs
+++ Checha/Assets/Scripts/Fade.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class Fade : MonoBehaviour
 {
-    public Animator anim_canvas;
+    public Animator AnimCanvas;
 
     public void FadeIn()
     {
-        anim_canvas.SetBool("fade", true);
+        AnimCanvas.SetBool("fade", true);
     }
 
     public void FadeOut()
     {
-        anim_canvas.SetBool("fade", false);
+        AnimCanvas.SetBool("fade", false);
     }
 }
